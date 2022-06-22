94268fdc45e73b229c6315e8de62511cf333ea15
DESKTOP-EVEK51B\Hudi <wi21b053@technikum-wien.at>
2022-06-22 19:49:37 +0200

    Initial commit

---
+++ src/main/java/at/technikum/timeapi/TimeApiApplication.java
@@ -0,0 +1,13 @@
+package at.technikum.timeapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimeApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TimeApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/at/technikum/timeapi/TimeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package at.technikum.timeapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimeApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
