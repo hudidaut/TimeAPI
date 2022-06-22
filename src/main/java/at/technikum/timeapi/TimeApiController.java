0a25a87560a6f7a2ef189d6c1432491795b6e669
DESKTOP-EVEK51B\Hudi <wi21b053@technikum-wien.at>
2022-06-22 20:52:12 +0200

    implemented now feature

--- src/main/java/at/technikum/timeapi/TimeApiController.java
+++ src/main/java/at/technikum/timeapi/TimeApiController.java
@@ -4,13 +4,21 @@ package at.technikum.timeapi;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import java.time.LocalDate;
+import java.time.LocalTime;
+import org.springframework.web.bind.annotation.GetMapping;
 
 
 @RestController
 public class TimeApiController {
-    @RequestMapping("/today")
+    @GetMapping("today")
     public LocalDate date (){
         LocalDate d = LocalDate.now();
         return d;
     }
+    @GetMapping("now")
+    public String now(){
+        LocalTime now = LocalTime.now();
+        return now.toString();
+    }
+
 }
